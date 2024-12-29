62a1427a4924b20ede009679cf554d30cdd5e396
kalyan <pgmounikareddy2022@gmail.com>
2024-12-29 20:25:38 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@
 
 PERSON1=Ramesh
 PERSON2=Suresh
-echo "$PERSON1:: hello PERSON2.How are you?"
-echo "$PERSON2:: hello PERSON1.I am fine.How are you?"
+echo "$PERSON1:: hello $PERSON2.How are you?"
+echo "$PERSON2:: hello $PERSON1.I am fine.How are you?"
 echo  "$PERSON1:: I am fine too.How is your work?"
 echo "$PERSON2:: Not bad.im thinking to upgrade to devops "
