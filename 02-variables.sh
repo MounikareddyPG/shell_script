cecf9619d909d47f3dddd5d743e54d52a5c90bc0
kalyan <pgmounikareddy2022@gmail.com>
2024-12-29 20:05:43 +0530

    why you commited

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hello suresh.How are you?"
+echo "Suresh:: hello ramesh.I am fine.How are you?"
+echo  "Ramesh:: I am fine too.How is your work?"
+echo " Suresh:: Not bad.im thinking to upgrade to devops "
