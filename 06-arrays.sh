f8b7019ebb5ab7788bb1b945eba652d2e2d8ad7f
kalyan <pgmounikareddy2022@gmail.com>
2025-01-06 17:49:59 +0530

    some message

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Movies=("RRR","Murari","DJTILLU","MUFASA")
+movies=("RRR","Murari","DJTILLU","MUFASA")
 #size of array is 4
 #index starts with 0
-echo "first movie is : ${movies[0]}"
-echo "first movie is : ${movies[1]}"
-echo "first movie is : ${movies[2]}"
+echo "first movie is: ${movies[0]}"
+echo "first movie is: ${movies[1]}"
+echo "first movie is: ${movies[2]}"
