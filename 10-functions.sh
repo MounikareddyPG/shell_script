6d60021c9bbfedf0ce6c60e8a32e85e92500006e
kalyan <pgmounikareddy2022@gmail.com>
2025-01-21 10:44:48 +0530

    some message

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,7 +2,9 @@
 
 
 USERID=$(id -u)
-
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/#SCRIPT_NAME#TIMESTAMP.log
 
 VALIDATE (){
     if [ $1 -ne 0 ]
@@ -23,11 +25,11 @@ fi
 
 
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 
 VALIDATE $? "installing MYSQL"
 
-dnf install git -y
+dnf install git -y &>>LOGFILE
 
 VALIDATE $? "installing GIT"
 
