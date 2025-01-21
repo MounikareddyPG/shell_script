932e87f10dc983933df3262d71a864553a26e499
kalyan <pgmounikareddy2022@gmail.com>
2025-01-21 10:46:49 +0530

    some message

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,7 +3,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/#SCRIPT_NAME#TIMESTAMP.log
 
 VALIDATE (){
@@ -14,7 +14,7 @@ VALIDATE (){
     else 
        echo "$2..SUCCESS"
     fi
-}
+    }
 if [ $USERID -ne 0 ]
 then 
    echo "please run this script with root access"
