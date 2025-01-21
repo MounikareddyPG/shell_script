cd81224baf6f15db6854a8df5b90e482a0bf5c9f
kalyan <pgmounikareddy2022@gmail.com>
2025-01-21 09:23:46 +0530

    some message

--- 09-installation.sh
+++ 09-installation.sh
@@ -7,9 +7,14 @@ if [ $USERID -ne 0 ]
 
 then 
 echo "please run this script with root access"
+
+exit 1
 else
 echo "you are super user"
 fi
+
+
+
 dnf install mysql -y
 
 echo "Is script proceeding?"
